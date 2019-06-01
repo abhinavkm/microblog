3deb9c7aecf0453b6d9baf67c72d57507f25460c
Abhinav Mulagada <abhinav.abhi96@gmail.com>
2019-06-01 10:11:18 -0700

    partially done with requiring users to login

--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,31 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    user = {'username':'Abhinav'}
-    posts = [
-        {
-            'author': {'username':'John'},
-            'body':'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username':'Susan'},
-            'body':'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html',title='Home', user=user, posts = posts)
+    # user = {'username':'Abhinav'}
+    # posts = [
+    #     {
+    #         'author': {'username':'John'},
+    #         'body':'Beautiful day in Portland!'
+    #     },
+    #     {
+    #         'author': {'username':'Susan'},
+    #         'body':'The Avengers movie was so cool!'
+    #     }
+    # ]
+    return render_template('index.html',title='Home', posts = posts)
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
